3c368e609c8a5ba0f1d908bcb1589d028cced8a0
Ejjonzey <ejjonzey1982@gmail.com>
2018-01-25 21:00:55 -0500

    apollo server connected

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Meteor} from 'meteor/meteor';
 import {render} from 'react-dom';
 
-import App from '../../ui/app';
+import App from '../../ui/App';
 
 Meteor.startup(() => {
     render(<App />, document.getElementById('app'))
